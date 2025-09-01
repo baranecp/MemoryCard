d52a4bda8e191dd569e7818801f48056d6272175
baranecp <baranec.dev@gmail.com>
2025-09-01 14:39:45 +0200

    Add state for tracking the score and high score

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -4,6 +4,8 @@ import PokemonCard from "./PokemonCard";
 function PokemonCards() {
 
     const [pokemons, setPokemons] = useState([]);
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=12&offset=0')
@@ -34,20 +36,43 @@ function PokemonCards() {
 
     const handlePokemonClick = (name) => {
         setPokemons(prev => {
-            const updated = prev.map(pokemon =>
-                pokemon.name === name
-                    ? { ...pokemon, isClicked: true }
-                    : pokemon
-            );
-            return shuffleArray(updated); // shuffle after updating
-        });
-    };
+            const clickedPokemon = prev.find(pokemon => pokemon.name === name);
+            if (clickedPokemon.isClicked) {
+                setHighScore(prevHigh => Math.max(prevHigh, score))
+                setScore(0)
+
+                const resetPokemons = prev.map(pokemon => ({ ...pokemon, isClicked: false }))
+                return shuffleArray(resetPokemons)
+            } else {
+                setScore(score + 1)
+                const updated = prev.map(pokemon =>
+                    pokemon.name === name
+                        ? { ...pokemon, isClicked: true }
+                        : pokemon
+                );
+                return shuffleArray(updated);
+            }
+        })
+    }
+
 
     return (
-        <div className="pokemons">
-            {pokemons.map((pokemon) => (
-                <PokemonCard key={pokemon.id} onClick={() => handlePokemonClick(pokemon.name)} alt={pokemon.name} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
-            ))}
+        <div>
+            <div className="scoreboard">
+                <p>Score: {score}</p>
+                <p>High Score: {highScore}</p>
+            </div>
+
+            <div className="pokemon-grid">
+                {pokemons.map(pokemon => (
+                    <PokemonCard
+                        key={pokemon.id}
+                        onClick={() => handlePokemonClick(pokemon.name)}
+                        alt={pokemon.name}
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                    />
+                ))}
+            </div>
         </div>
     )
 
